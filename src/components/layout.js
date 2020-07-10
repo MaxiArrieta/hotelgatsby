13e67d204401fd3fd1da04b642cf042135ea1d01
MaxiArrieta <arrieta.maxi18@gmail.com>
2020-07-10 19:16:29 -0300

    habitaciones con gatsby-node

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,26 +10,35 @@ const Layout = props => {
       <Global
         //Definimos el css global
         styles={css`
-        html {
-          font-size: 62.5%;
-        }  
-        body {
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+          body {
             font-size: 18px;
-            font-size: 1.8rem
+            font-size: 1.8rem;
             line-height: 1.5;
             font-family: "PT Sans", sans-serif;
           }
-          h1,h2,h3{
+          h1,
+          h2,
+          h3 {
             margin: 0;
             line-height: 1.5;
           }
-          h1,h2{
+          h1,
+          h2 {
             font-family: "Roboto", serif;
           }
-          h3{
+          h3 {
             font-family: "PT Sans", sans-serif;
           }
-          ul{
+          ul {
             list-style: none;
             margin: 0;
             padding: 0;
