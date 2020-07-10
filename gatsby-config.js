d43a899671828aa8e97a636bca6897e883237289
MaxiArrieta <arrieta.maxi18@gmail.com>
2020-07-09 22:43:20 -0300

    cargando paginas con DatoCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,11 @@ module.exports = {
         //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `42757f2c05033d5a1d7392828025e5`,
+      },
+    },
   ],
 }
